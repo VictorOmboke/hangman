7e6716b7b0904f7bcfab6a9d2fd2131b9872a1ef
Victor Omboke <victoromboke@gmail.com>
2025-03-16 21:01:27 -0700

    Add function to randomly select a word from a list of words.

--- hangman.py
+++ hangman.py
@@ -1,2 +1,15 @@
+#Import list of words from words.py.
 from words import words
+#Import random module.
 import random
+
+#Randomly select a word from the words list.
+def word_selector():
+    """Select a random word from the list of words.
+
+    Returns:
+        random_word: (string) - a random word from the words list.
+    """
+    #Randomly select and store any word from the words list.
+    random_word = random.choice(words)
+    return random_word
