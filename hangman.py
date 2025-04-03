1eafa4b25be100b86554e48d952ca6cfa3053ab0
Victor Omboke <victoromboke@gmail.com>
2025-04-03 11:21:54 -0700

    Fix variable name for clarity.

--- hangman.py
+++ hangman.py
@@ -1,5 +1,5 @@
 #Import list of words from words.py.
-from words import words
+from words import words_list
 #Import random module.
 import random
 
@@ -11,5 +11,5 @@ def word_selector():
         random_word: (string) - a random word from the words list.
     """
     #Randomly select and store any word from the words list.
-    random_word = random.choice(words)
+    random_word = random.choice(words_list)
     return random_word
--- words.py
+++ words.py
@@ -1,1 +1,1 @@
-words = ["python", "hangman", "playful", "sunset", "programming", "car", "jazz", "rhythm", "doctor", "mercedes"]
+words_list = ["python", "hangman", "playful", "sunset", "programming", "car", "jazz", "rhythm", "doctor", "mercedes"]
